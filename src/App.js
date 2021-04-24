3162b6a538d821e29bf4e11e38270cf3d39d3498
joshua <joshua.cattaruzza@gmail.com>
2021-04-25 00:46:45 +0200

    "Esercizio todo-list e fetch API"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+function App () {
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState({
+    title: "",
+    description: ""
+  })
+const [isloading, setIsLoading] = useState(false);
+const [movies, setMovies] = useState([])
 
-function App() {
+useEffect(() => {
+  fetchMovies();
+}, [])
+
+  //funzione che aggiunge il task. Richiamo setTasks e passo
+  //come param prev che é l'array contente i task precedenti(?) e ritorna l'array con prev "spreaddato" + il currentTask aggiunto alla fine 
+  const addTask = () =>{
+    setTasks((prev) => {
+      return [...prev, currentTask];
+    })
+  }
+  const deleteTask = (index) =>{
+    setTasks((prev) => prev.filter((_, idx) => index !== idx));
+  }
+  const fetchMovies = () =>{
+    setIsLoading(true);
+    fetch("https://swapi.dev/api/films/")
+    .then(res=>
+    res.json()
+    )
+    .then((json) => {
+      setMovies(json.results)
+    })
+    .catch(err => console.log(err))
+    .finally(setIsLoading(false))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       {movies.map((el) => (
+        <>
+        <ul>
+          <li>
+          <h1>{el.title}</h1>
+          <h2>{el.director}</h2>
+          <h3>{el.release_date}</h3>
+          </li>
+          </ul>
+        </>
+      ))}
+
+      <Task
+      value={currentTask.title} 
+      onChange={(val) =>
+         setCurrentTask((prev) =>
+         ({
+        ...prev,
+        title: val
+      })
+      )}
+      />
+      <Task
+      value={currentTask.description} 
+      onChange={(val) =>
+         setCurrentTask((prev) =>
+         ({
+        ...prev,
+        description: val
+      })
+      )}
+      />
+      <button
+      onClick={() => addTask()}>Add a Task</button>
+
+      <TaskList
+       el={tasks}
+       removeTask={(index) => deleteTask(index)}
+      />
     </div>
   );
 }
+
+//Questo è il componente che andrà a contenere i task aggiunti alla lista
+//prende come param un obj con due elementi "value" e "onChange"(func in cui salvo l'onchange dell'input
+const Task = ({value, onChange }) => (
+  <input 
+  type="text"
+  value={value}
+  onChange={(e) => onChange(e.target.value)}
+  >
+  </input>
+);
+const TaskList = ({el, removeTask}) =>(
+  <>
+   <h1>Things to do: {el.length}</h1>
+   {
+     el.map((el, i) => ( 
+       <div>
+        <h1>
+          {el.title}
+        </h1>
+        <h2>
+        {el.description}
+      </h2>
+      <button
+      onClick={()=> removeTask(i)}
+      >
+        Done
+      </button>
+      </div>
+     ))
+   }
+  </>
+)
+
 
 export default App;
